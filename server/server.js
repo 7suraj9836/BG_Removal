d64674f5bdc80cb717ad75414f3eab7e0bd7879c
surajsingh139 <suraj.singh1@mail.vinove.com>
2024-10-27 23:30:06 +0530

    app listen message improved

--- server/server.js
+++ server/server.js
@@ -17,4 +17,4 @@ app.get('/',(req,res)=>{
   res.send("Api is working");
 })
 
-app.listen(PORT,()=>console.log('Server running at port'+PORT));
+app.listen(PORT,()=>console.log('Server running at port '+PORT));
