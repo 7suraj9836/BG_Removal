f870ceb6742a65a350debd173ff699eebb6348b9
surajsingh139 <suraj.singh1@mail.vinove.com>
2024-10-28 22:49:29 +0530

    fixed syntax error of svix properties in userController

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -5,13 +5,14 @@ import userModel from "../models/userModel.js";
 // http://localhost:4000/api/user/webhooks
 
 const clerkWebhooks = async (req, res) => {
+    console.log("req",req);
   try {
     //create a svix instance with clerk webhook secret
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
     await whook.verify(JSON.stringify(req.body), {
-      "svix-id": req.headers("svix-id"),
-      "svix-timestamp": req.headers("svix-timestamp"),
-      "svix-signature": req.headers("svix-signature"),
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
     });
        const {data,type}=req.body;
        switch (type) {
@@ -24,6 +25,7 @@ const clerkWebhooks = async (req, res) => {
                 lastName:data.last_name,
                 }
             await userModel.create(userData);
+            console.log(res,"res")
             res.json({});
             break;
         }
